1ee6c0a0386f400ba9a9e0e85e0370afa3f55abd
pepedd864 <pepedd@qq.com>
2023-12-10 11:56:00 +0800

    feat: init template

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+import zhCN from 'ant-design-vue/es/locale/zh_CN'
+import { theme } from 'ant-design-vue'
+import { nextTick } from 'vue'
+
+const themeConfig = {
+  algorithm: theme.defaultAlgorithm,
+}
+
+// 加载完成
+nextTick(() => {
+  setTimeout(() => document.dispatchEvent(new CustomEvent('loaded')), 2000)
+})
+</script>
+
+<template>
+  <a-config-provider :locale="zhCN" :theme="themeConfig">
+    <router-view />
+  </a-config-provider>
+</template>
+
+<style lang="scss" scoped></style>
