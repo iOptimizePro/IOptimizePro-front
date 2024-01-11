59ea2c7211dd9bc920e71c32fadee262300ca433
pepedd864 <pepedd@qq.com>
2024-01-11 12:10:42 +0800

    style: 重新格式化代码

--- src/layouts/components/IBackground/v1/PerlinNoise/PerlinNoise.ts
+++ src/layouts/components/IBackground/v1/PerlinNoise/PerlinNoise.ts
@@ -53,164 +53,162 @@ class Particle {
 }
 
 export default class PerlinNoise {
-    private particles: Particle[] = []
-    private maxLife: number
-    private options: any
-    private dom: HTMLElement | null = document.querySelector('.perlinNoise')
-    private timer: any
-    private p5: any
+  private particles: Particle[] = []
+  private maxLife: number
+  private options: any
+  private dom: HTMLElement | null = document.querySelector('.perlinNoise')
+  private timer: any
+  private p5: any
 
-    constructor(dom: string, options: any) {
-        this.dom = document.querySelector(dom)
-        this.options = options
-        this.p5 = new p5(this.sketch)
+  constructor(dom: string, options: any) {
+    this.dom = document.querySelector(dom)
+    this.options = options
+    this.p5 = new p5(this.sketch)
+  }
+
+  private sketch = (p: p5) => {
+    let backgroundColor: any
+
+    p.setup = () => {
+      this.timer && clearInterval(this.timer)
+      backgroundColor = p.color(this.options.Background)
+      const canvas = p.createCanvas(p.windowWidth, p.windowHeight) as p5.Renderer
+      canvas.parent(this.dom as HTMLElement)
+      p.background(this.options.Background)
+      for (let i = 0; i < this.options.Nums; i++) {
+        this.particles[i] = new Particle(p, this.options)
+      }
+
+      this.timer = setInterval(() => {
+        p.resetParticles()
+      }, 60000)
     }
 
-    private sketch = (p: p5) => {
-        let backgroundColor: any
+    p.draw = () => {
+      p.noStroke()
+      p.smooth()
 
-        p.setup = () => {
-            this.timer && clearInterval(this.timer)
-            backgroundColor = p.color(this.options.Background)
-            const canvas = p.createCanvas(p.windowWidth, p.windowHeight) as p5.Renderer
-            canvas.parent(this.dom as HTMLElement)
-            p.background(this.options.Background)
-            for (let i = 0; i < this.options.Nums; i++) {
-                this.particles[i] = new Particle(p, this.options)
+      this.maxLife = this.options.Length
+      for (let i = 1; i < this.options.Nums; i++) {
+        const iterations = p.map(i, 0, this.options.Nums, 5, 1)
+        const radius = this.options.Size
+
+        this.particles[i].move(iterations)
+        this.particles[i].checkEdge()
+
+        const alpha = 255
+        let particleColor
+        let fadeRatio
+        fadeRatio = p.min((this.particles[i].life * 5) / this.maxLife, 1)
+        fadeRatio = p.min(((this.maxLife - this.particles[i].life) * 5) / this.maxLife, fadeRatio)
+        const lifeRatioGrayscale = p.min(255, (255 * this.particles[i].life) / this.maxLife + p.red(backgroundColor))
+
+        if (this.options.ColorMode === 'Normal') {
+          if (i % 3 === 0) {
+            particleColor = this.options.Color1
+          }
+          if (i % 3 === 1) {
+            particleColor = this.options.Color2
+          }
+          if (i % 3 === 2) {
+            particleColor = this.options.Color3
+          }
+        }
+
+        if (this.options.ColorMode === 'Linear Gradient') {
+          const percent1 = p.norm(this.particles[i].pos.x, 0, p.width / 2)
+          const percent2 = p.norm(this.particles[i].pos.x, p.width / 2, p.width)
+          const from = p.color(this.options.Color1)
+          const middle = p.color(this.options.Color2)
+          const to = p.color(this.options.Color3)
+          const between1 = p.lerpColor(from, middle, percent1)
+          const between2 = p.lerpColor(middle, to, percent2)
+
+          if (this.particles[i].pos.x > 0 && this.particles[i].pos.x < p.width / 2) {
+            particleColor = between1
+          } else {
+            particleColor = between2
+          }
+        }
+
+        if (this.options.ColorMode === 'Radial Gradient') {
+          const distance = p.dist(this.particles[i].pos.x, this.particles[i].pos.y, p.width / 2, p.height / 2)
+          const percent1 = p.norm(distance, 0, 400)
+          const percent2 = p.norm(distance, 400, p.width / 2)
+          const from = p.color(this.options.Color1)
+          const middle = p.color(this.options.Color2)
+          const to = p.color(this.options.Color3)
+          const between1 = p.lerpColor(from, middle, percent1)
+          const between2 = p.lerpColor(middle, to, percent2)
+
+          if (distance < 400) {
+            particleColor = between1
+          } else {
+            particleColor = between2
+          }
+        }
+
+        if (this.options.ColorMode === 'Splice') {
+          if (this.particles[i].pos.x >= p.width / 3 && this.particles[i].pos.x <= (p.width / 3) * 2) {
+            if (i % 3 === 0) {
+              particleColor = this.options.Color1
             }
-
-            this.timer = setInterval(() => {
-                p.resetParticles()
-            }, 60000)
-        }
-
-        p.draw = () => {
-            p.noStroke()
-            p.smooth()
-
-            this.maxLife = this.options.Length
-            for (let i = 1; i < this.options.Nums; i++) {
-                const iterations = p.map(i, 0, this.options.Nums, 5, 1)
-                const radius = this.options.Size
-
-                this.particles[i].move(iterations)
-                this.particles[i].checkEdge()
-
-                const alpha = 255
-                let particleColor
-                let fadeRatio
-                fadeRatio = p.min((this.particles[i].life * 5) / this.maxLife, 1)
-                fadeRatio = p.min(((this.maxLife - this.particles[i].life) * 5) / this.maxLife, fadeRatio)
-                const lifeRatioGrayscale = p.min(255, (255 * this.particles[i].life) / this.maxLife + p.red(backgroundColor))
-
-                if (this.options.ColorMode === 'Normal') {
-                    if (i % 3 === 0) {
-                        particleColor = this.options.Color1
-                    }
-                    if (i % 3 === 1) {
-                        particleColor = this.options.Color2
-                    }
-                    if (i % 3 === 2) {
-                        particleColor = this.options.Color3
-                    }
-                }
-
-                if (this.options.ColorMode === 'Linear Gradient') {
-                    const percent1 = p.norm(this.particles[i].pos.x, 0, p.width / 2)
-                    const percent2 = p.norm(this.particles[i].pos.x, p.width / 2, p.width)
-                    const from = p.color(this.options.Color1)
-                    const middle = p.color(this.options.Color2)
-                    const to = p.color(this.options.Color3)
-                    const between1 = p.lerpColor(from, middle, percent1)
-                    const between2 = p.lerpColor(middle, to, percent2)
-
-                    if (this.particles[i].pos.x > 0 && this.particles[i].pos.x < p.width / 2) {
-                        particleColor = between1
-                    } else {
-                        particleColor = between2
-                    }
-                }
-
-                if (this.options.ColorMode === 'Radial Gradient') {
-                    const distance = p.dist(this.particles[i].pos.x, this.particles[i].pos.y, p.width / 2, p.height / 2)
-                    const percent1 = p.norm(distance, 0, 400)
-                    const percent2 = p.norm(distance, 400, p.width / 2)
-                    const from = p.color(this.options.Color1)
-                    const middle = p.color(this.options.Color2)
-                    const to = p.color(this.options.Color3)
-                    const between1 = p.lerpColor(from, middle, percent1)
-                    const between2 = p.lerpColor(middle, to, percent2)
-
-                    if (distance < 400) {
-                        particleColor = between1
-                    } else {
-                        particleColor = between2
-                    }
-                }
-
-                if (this.options.ColorMode === 'Splice') {
-                    if (this.particles[i].pos.x >= p.width / 3 && this.particles[i].pos.x <= (p.width / 3) * 2) {
-                        if (i % 3 === 0) {
-                            particleColor = this.options.Color1
-                        }
-                        if (i % 3 === 1) {
-                            particleColor = this.options.Color2
-                        }
-                        if (i % 3 === 2) {
-                            particleColor = this.options.Color3
-                        }
-                    } else if (this.particles[i].pos.x < p.width / 3) {
-                        if (i % 3 === 0) {
-                            particleColor = p.color(20)
-                        }
-                        if (i % 3 === 1) {
-                            particleColor = p.color(100)
-                        }
-                        if (i % 3 === 2) {
-                            particleColor = p.color(220)
-                        }
-                    } else if (this.particles[i].pos.x > (p.width / 3) * 2) {
-                        if (i % 3 === 0) {
-                            particleColor = p.color(
-                                255 - p.red(this.options.Color1),
-                                255 - p.green(this.options.Color1),
-                                255 - p.blue(this.options.Color1),
-                            )
-                        }
-                        if (i % 3 === 1) {
-                            particleColor = p.color(
-                                255 - p.red(this.options.Color2),
-                                255 - p.green(this.options.Color2),
-                                255 - p.blue(this.options.Color2),
-                            )
-                        }
-                        if (i % 3 === 2) {
-                            particleColor = p.color(
-                                255 - p.red(this.options.Color3),
-                                255 - p.green(this.options.Color3),
-                                255 - p.blue(this.options.Color3),
-                            )
-                        }
-                    }
-                }
-
-                p.fill(p.red(particleColor), p.green(particleColor), p.blue(particleColor), alpha * fadeRatio)
-                this.particles[i].display(radius)
+            if (i % 3 === 1) {
+              particleColor = this.options.Color2
             }
+            if (i % 3 === 2) {
+              particleColor = this.options.Color3
+            }
+          } else if (this.particles[i].pos.x < p.width / 3) {
+            if (i % 3 === 0) {
+              particleColor = p.color(20)
+            }
+            if (i % 3 === 1) {
+              particleColor = p.color(100)
+            }
+            if (i % 3 === 2) {
+              particleColor = p.color(220)
+            }
+          } else if (this.particles[i].pos.x > (p.width / 3) * 2) {
+            if (i % 3 === 0) {
+              particleColor = p.color(
+                255 - p.red(this.options.Color1),
+                255 - p.green(this.options.Color1),
+                255 - p.blue(this.options.Color1),
+              )
+            }
+            if (i % 3 === 1) {
+              particleColor = p.color(
+                255 - p.red(this.options.Color2),
+                255 - p.green(this.options.Color2),
+                255 - p.blue(this.options.Color2),
+              )
+            }
+            if (i % 3 === 2) {
+              particleColor = p.color(
+                255 - p.red(this.options.Color3),
+                255 - p.green(this.options.Color3),
+                255 - p.blue(this.options.Color3),
+              )
+            }
+          }
         }
 
-        p.resetParticles = () => {
-            for (let i = 0; i < this.options.Nums; i++) {
-                this.particles[i].respawn()
-                this.particles[i].life = p.random(0, this.maxLife)
-            }
-            p.resizeCanvas(p.windowWidth, p.windowHeight)
-            p.background(this.options.Background)
-        }
-        p.windowResized = () => {
-            p.resetParticles()
-        }
+        p.fill(p.red(particleColor), p.green(particleColor), p.blue(particleColor), alpha * fadeRatio)
+        this.particles[i].display(radius)
+      }
     }
+
+    p.resetParticles = () => {
+      for (let i = 0; i < this.options.Nums; i++) {
+        this.particles[i].respawn()
+        this.particles[i].life = p.random(0, this.maxLife)
+      }
+      p.resizeCanvas(p.windowWidth, p.windowHeight)
+      p.background(this.options.Background)
+    }
+    p.windowResized = () => {
+      p.resetParticles()
+    }
+  }
 }
-
-
